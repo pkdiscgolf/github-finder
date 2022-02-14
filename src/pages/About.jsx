c5f0b6cc998812778b78fae1bbe2f3ca82cd00e7
pkdiscgolf <kellypatrick1986@gmail.com>
2022-02-13 19:09:14 -0600

    end of 53

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className='text-6xl mb-4'>
+            About this app
+        </h1>
+        
+    </div>
+  )
+}
+
+export default About
